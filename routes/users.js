16b6c946f04d8cf7ab51419d93f40d6dbfc19215
Hannes Porombka <hannes.porombka@gmail.com>
2024-01-07 18:02:13 +0100

    last update 070124

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const User = require("../models/user.js")
+const {UnauthorizedError} = require("../expressError.js")
 const router = new express.Router();
 const db = require("../db")
+const userUpdateSchema = require("../schemas/userUpdateSchema.json")
 const { ensureLoggedIn, authenticateJWT, ensureAdmin } = require("../middleware/auth");
 
 
@@ -18,11 +20,29 @@ catch(e){
 
 router.get("/:username", ensureLoggedIn, async function (req, res, next) {
     try {
-        console.log(req.params)
       if (req.params.username != res.locals.user.username && !res.locals.user.isAdmin){
         throw new UnauthorizedError
       }
       const user = await User.get(req.params.username);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+
+  router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
+    try {
+      if (req.params.username != res.locals.user.username && !res.locals.user.isAdmin){
+        throw new UnauthorizedError
+      }
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const user = await User.update(req.params.username, req.body);
       return res.json({ user });
     } catch (err) {
       return next(err);
